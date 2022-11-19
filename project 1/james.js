7fe10cde2bda571f462531fe33f8e1e8edaeecd7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-19 02:13:05 -0800

    symbol created for the key fred

--- project 1/james.js
+++ project 1/james.js
@@ -1,17 +1,2 @@
-let mySymbol = Symbol('description');
-
-console.log(mySymbol);
-
-let email = Symbol();
-let employee = {};
-employee.name = 'james';
-employee.age = 53;
-employee[email] = 'james@gmail.com';
-console.log(Object.keys(employee));
-
-for (const key in employee) {
-    console.log(key);
-}
-
-console.log(employee[email]);
-console.log(employee);
+let s4 = Symbol.for('fred')
+console.log(s4);
